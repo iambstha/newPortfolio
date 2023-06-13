8f098c2220f3d6c03f4a12eeaa3bbb643b8dce20
iambstha <iambstha@gmail.com>
2023-06-13 21:50:04 +0545

    Made header responsive

--- components/core/Header.js
+++ components/core/Header.js
@@ -6,11 +6,11 @@ import Link from 'next/link'
 const Header = () => {
     return (
         <div className=' p-2 '>
-            <div className=' flex justify-between items-center ' >
+            <div className=' flex flex-col md:flex-row justify-between items-center ' >
                 <div className=' felx flex-col '>
                     <Link href='/'><Image src='/next.svg' width={200} height={200} /></Link>
                 </div>
-                <div className=' felx flex-col '>
+                <div className=' felx flex-col p-2 md:p-0 '>
                     <Nav />
                 </div>
             </div>
