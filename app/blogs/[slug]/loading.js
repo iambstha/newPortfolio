16d7665ad4d80f5983b8af35e254802531bd4d8a
iambstha <iambstha@gmail.com>
2023-06-14 18:25:19 +0545

    Made loading page for post

---
+++ app/blogs/[slug]/loading.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const page = ({  }) => {
+
+    return (
+        <div className=' flex w-full justify-center items-center ' >
+            <div className=' p-2 px-6 pt-8 flex gap-4 flex-col w-full md:w-2/3  ' >
+                <h2 className=' text-3xl md:text-4xl lg:text-6xl text-slate-950 leading-10 pt-2 w-full h-10 bg-slate-300 ' ></h2>
+                <h3 className=' text-base md:text-xl text-slate-500 pb-4  w-full h-4 bg-slate-300  ' ></h3>
+                <p>
+                <div className=' h-80 w-full bg-slate-300' ></div>
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+export default page
